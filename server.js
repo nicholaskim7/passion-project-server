02c88303d7de223b3c6198c23fa1ac1a59693ea9
nicholaskim7 <nickdraggy@gmail.com>
2025-06-22 22:23:00 -0500

    database connection and .env file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+require('dotenv').config();
+const mysql = require("mysql2");
 const cors = require("cors");
 const corsOptions = {
   origin: ["http://localhost:5173"], // only accept requests from our frontend server
@@ -9,10 +11,25 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('DB connection was successful');
+
+});
+
 app.post("/api/log-workout", (req, res) => {
-  console.log(req.body);
-  let {exercises} = req.body;
-  res.json({"message": "Form Submitted"});
+  console.log(req.body); //req received from client  example benchpress : {set 1:[reps, weight], set 2:[reps, weight], set3........}
+  let {exercises} = req.body; 
+  res.json({"message": "Form Submitted"}); //response that client will receive
 });
 
 
