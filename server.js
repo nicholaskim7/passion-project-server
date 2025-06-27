0a9cb5d79824c56dcdf9aa2945ff310950d22d66
nicholaskim7 <nickdraggy@gmail.com>
2025-06-27 12:20:53 -0500

    cors configs

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const { Pool } = require("pg");
 const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:5173"], // only accept requests from our frontend server
+  origin: ["http://localhost:5173", "https://passion-project-client.vercel.app"], // only accept requests from our frontend server or hosted server
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
